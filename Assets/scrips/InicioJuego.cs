b9438b63ab9c4d71bb400e815da91a73eed501a1
es12pha27 <esurcoa@unsa.edu.pe>
2021-12-23 13:03:40 -0500

    Disenio del juego parte menuprincipal

---
+++ Assets/scrips/InicioJuego.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class InicioJuego : MonoBehaviour
+{
+    public void Iniciar(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+   public void Salir(){
+       Debug.Log("QUIT");
+       Application.Quit();
+   }
+}
